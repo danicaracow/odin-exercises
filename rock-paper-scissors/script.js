b585b6d81c4346739a68e5965e818ca5beac53a8
danicaracow <79090619+danicaracow@users.noreply.github.com>
2024-07-08 15:02:22 +0200

    Add computer random choice getter

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3);
+    switch(num){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+
+
+console.log(getComputerChoice());
